13a89d4c23383b8a51f8eba0e8d5bc72b1607614
AustinC02 <Austin.m.Carrington@gmail.com>
2024-01-04 19:30:18 -0600

    Did some mobile formatting sponsors page now looks good

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
             <div className="">
                 <iframe
                     src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%230B8043&ctz=America%2FChicago&showTitle=1&showPrint=0&showTabs=1&title=Staley%20Robotics%20Calendar&showDate=1&showNav=1&showCalendars=0&showTz=0&src=c3RhbGV5cm9ib3RpY3NAZ21haWwuY29t&color=%237986CB"
-                    style={{border: "solid 1px #777"}} width="800vw" height="600vh" title={"Calendar"}>
+                    style={{border: "solid 1px #777"}} width="900" height="675" title={"Calendar"} className="fixIframe">
                 </iframe>
             </div>
         </div>
--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <div className="">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2837.0996291106194!2d-94.54590295669351!3d39.28987882079203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0576f54c35871%3A0x1a6b3f59cf67a0fa!2sStaley%20High%20School!5e0!3m2!1sen!2sus!4v1701886467380!5m2!1sen!2sus"
-                    width="600" height="450" style={{border: 0}} loading="eager"
+                    width="600" height="450" style={{border: "0"}} loading="eager" className="fixIframe"
                     referrerPolicy="no-referrer-when-downgrade" title={"Staley Location Map"}>
 
                 </iframe>
