11640e3264edd58e02e6c27e93d9466450ae6c25
AustinC02 <Austin.m.Carrington@gmail.com>
2024-01-30 12:40:54 -0600

    Boom

--- src/Util/Effects/3D/index.tsx
+++ src/Util/Effects/3D/index.tsx
@@ -2,13 +2,8 @@
 import {Float, OrbitControls} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import "../../../App.css";
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-
+import {MDrew} from "../../../MDrew";
 export default () => {
-    const gltf = useLoader(GLTFLoader, '/media/seasons/2023/model/Drew.gltf')
-
     return (
         <Canvas>
             <ambientLight/>
@@ -17,8 +12,7 @@ export default () => {
             <pointLight position={[0, 15, 0]} intensity={1000}/>
             <OrbitControls/>
             <Float speed={1.4} rotationIntensity={0} floatIntensity={0}>
-                <primitive object={gltf.scene}></primitive>
-                {/*<Drew position={[0,-7.5,0]}/>*/}
+                <MDrew position={[0,-7.5,0]}/>
             </Float>
         </Canvas>
     );
