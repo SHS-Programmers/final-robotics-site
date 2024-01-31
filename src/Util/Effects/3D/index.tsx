3a47c0f7392b76ab98773909a9e71eebe5c57e63
Hudson Stolfus <hudson.stolfus@gmail.com>
2024-01-31 11:29:34 -0600

    GBL loading fix

--- src/Util/Effects/3D/index.tsx
+++ src/Util/Effects/3D/index.tsx
@@ -3,15 +3,19 @@ import {Canvas, useLoader} from "@react-three/fiber";
 import "../../../App.css";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
+import {startTransition} from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props: { glb: string }) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderConfig({ type: 'js' });
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-    const gltf = useLoader(GLTFLoader, props.glb, (loader) => {
-        loader.setDRACOLoader(dracoLoader);
-    });
+    let gltf: any = undefined;
+    startTransition(() => {
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderConfig({ type: 'js' });
+        dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+        gltf = useLoader(GLTFLoader, props.glb, (loader) => {
+            loader.setDRACOLoader(dracoLoader);
+        });
+    })
 
     return (
         <Canvas>
