66d3ad64ef287f14616aef213c1a4c30befafbbc
Matthew Chaffin <chaffinklan@outlook.com>
2024-02-20 14:49:08 -0600

    Did twitch things

--- src/Twitch/Twitch.tsx
+++ src/Twitch/Twitch.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { IframeHTMLAttributes } from 'react';
 import "./Twitch.css"
 const Twitch = () => {
     return (
         <div className="twitch">
             <iframe
-                title="Twitch Stream"
-                src="https://player.twitch.tv/?video=2024626809&parent=www.staleyrobotics.com">
-                frameborder="0"
-                scrolling="no"
-                height="100%"
-                width="100%"
-                autoplay=true
-            </iframe>
+                title="FRC Broadcast"
+                src="https://player.twitch.tv/?channel=firstinspires11&parent=localhost&parent=staleyrobotics.com&muted=true"
+                width="1920"
+                height="1080"          
+                allowFullScreen={true}
+            ></iframe>
         </div>
     );
 };
