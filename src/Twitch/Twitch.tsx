48b9a38662fdf9e6103741f75eb62026b4da5309
AustinC02 <Austin.m.Carrington@gmail.com>
2024-03-16 10:26:56 -0500

    fixed twitch stream

--- src/Twitch/Twitch.tsx
+++ src/Twitch/Twitch.tsx
@@ -5,7 +5,7 @@ const Twitch = () => {
         <div className="twitch">
             <iframe
                 title="FRC Broadcast"
-                src="https://player.twitch.tv/?channel=firstinspires11&parent=localhost&parent=www.staleyrobotics.com&muted=true"
+                src="https://player.twitch.tv/?channel=firstinspires4&parent=localhost&parent=www.staleyrobotics.com&muted=true"
                 width="100%"
                 height="100%"
                 allowFullScreen={true}
