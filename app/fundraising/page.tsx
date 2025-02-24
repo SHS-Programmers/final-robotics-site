d781b82cc5861f49facc1e1f8416a56077e7332d
Austin Carrington <austin.m.carrington@gmail.com>
2025-02-24 17:54:50 -0600

    Fixed responsive styling issues on Fundraising and home page

--- app/fundraising/page.tsx
+++ app/fundraising/page.tsx
@@ -1,66 +1,70 @@
 import React from "react";
-import Link from "next/link";
+            import Link from "next/link";
 
-const Fundraising = () => {
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-900 text-gray-300">
-            <div className="flex space-x-4">
-                <div className="bg-gray-800 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-lg w-full">
-                    <h2 className="text-2xl font-bold text-green-400 mb-4">Fundraising</h2>
-                    <strong className="block mb-4">
-                        The information below may not be accurate, and many fundraising options listed could be
-                        outdated. <br/>
-                        Please do{" "} <Link href="/contact"><span
-                        className="text-green-400 underline hover:text-green-300">contact</span> </Link>{" "} us if you
-                        would like to donate to our robotics program.
-                    </strong>
-                    <p className="leading-relaxed mb-4">
-                        Our goal is to raise $28,000 per year to operate our robotics program.
-                        <br/>
-                        <br/>
-                        This covers the cost of:
-                    </p>
-                    <ul className="list-disc list-inside mb-4 space-y-2">
-                        <li>Competition entry fees = $15,000</li>
-                        <li>Travel Expenses = $6,000</li>
-                        <li>Operating budget (shirts, tools, equipment, safety equip. etc.) = $2,000</li>
-                        <li>Robot parts and equipment = $5,000+</li>
-                    </ul>
-                    <p className="leading-relaxed mb-4">
-                        Please take a moment to visit our current fundraisers. Every bit supports our team:
-                    </p>
-                    <ul className="list-none space-y-2">
-                        <li>
-                            -{" "}
-                            <Link
-                                href="https://www.staleyfalconclub.com/falcon-club-memberships/"
-                                className="text-green-400 underline hover:text-green-300"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Falcon Club
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="bg-gray-800 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-lg w-full flex flex-col items-center">
-                            <h1 className="text-6xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
-                                NEW SHIRTS!
-                            </h1>
-                            <h3 className="text-lg font-medium text-gray-200 mt-2">
-                                Only available for a limited time!
-                            </h3>
-                            <h3 className={"text-lg font-medium text-gray-200 mt-2"}>
-                                (Closes 03/03/25 at 11:59PM)
-                            </h3>
-                            <a className="text-lg font-medium text-gray-200 bg-green-500 p-2 rounded-xl mt-6"
-                               href="https://staleyrobotics25.itemorder.com/">
-                                Order Now!
-                            </a>
+            const Fundraising = () => {
+                return (
+                    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-gray-300">
+                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+                            <div className="bg-gray-800 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-lg w-full order-2 md:order-1">
+                                <h2 className="text-2xl font-bold text-green-400 mb-4">Fundraising</h2>
+                                <strong className="block mb-4">
+                                    The information below may not be accurate, and many fundraising options listed could be
+                                    outdated. <br />
+                                    Please do{" "}
+                                    <Link href="/contact">
+                                        <span className="text-green-400 underline hover:text-green-300">contact</span>
+                                    </Link>{" "}
+                                    us if you would like to donate to our robotics program.
+                                </strong>
+                                <p className="leading-relaxed mb-4">
+                                    Our goal is to raise $28,000 per year to operate our robotics program.
+                                    <br />
+                                    <br />
+                                    This covers the cost of:
+                                </p>
+                                <ul className="list-disc list-inside mb-4 space-y-2">
+                                    <li>Competition entry fees = $15,000</li>
+                                    <li>Travel Expenses = $6,000</li>
+                                    <li>Operating budget (shirts, tools, equipment, safety equip. etc.) = $2,000</li>
+                                    <li>Robot parts and equipment = $5,000+</li>
+                                </ul>
+                                <p className="leading-relaxed mb-4">
+                                    Please take a moment to visit our current fundraisers. Every bit supports our team:
+                                </p>
+                                <ul className="list-none space-y-2">
+                                    <li>
+                                        -{" "}
+                                        <Link
+                                            href="https://www.staleyfalconclub.com/falcon-club-memberships/"
+                                            className="text-green-400 underline hover:text-green-300"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Falcon Club
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="bg-gray-800 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-lg w-full flex flex-col items-center order-1 md:order-2">
+                                <h1 className="text-6xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
+                                    NEW SHIRTS!
+                                </h1>
+                                <h3 className="text-lg font-medium text-gray-200 mt-2">
+                                    Only available for a limited time!
+                                </h3>
+                                <h3 className="text-lg font-medium text-gray-200 mt-2">
+                                    (Closes 03/03/25 at 11:59PM)
+                                </h3>
+                                <a
+                                    className="text-lg font-medium text-gray-200 bg-green-500 p-2 rounded-xl mt-6"
+                                    href="https://staleyrobotics25.itemorder.com/"
+                                >
+                                    Order Now!
+                                </a>
+                            </div>
                         </div>
-            </div>
-        </div>
-    );
-};
+                    </div>
+                );
+            };
 
-export default Fundraising;
+            export default Fundraising;
--- app/page.tsx
+++ app/page.tsx
@@ -14,37 +14,41 @@ const Landing: React.FC = () => {
         <div className="relative overflow-hidden">
             <div
                 className="relative bg-fixed bg-cover bg-center min-h-screen pt-0"
-                style={{ backgroundImage: "url('/path-to-bg.jpg')" }}
+                style={{backgroundImage: "url('/path-to-bg.jpg')"}}
             >
-                <TitleImage images={images} interval={4000} />
-                <div className={"absolute top-52 left-1/2 transform -translate-x-1/2 z-20"}>
-                    <div className="bg-gray-600 border-4 border-dashed border-green-400 p-12 rounded-xl flex-col flex items-center">
-                        <h1 className="text-6xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
+                <TitleImage images={images} interval={4000}/>
+                <div
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:top-52 md:-translate-y-0 z-20">
+                    <div
+                        className="bg-gray-600 border-4 border-dashed border-green-400 p-6 md:p-12 rounded-xl flex flex-col items-center">
+                        <h1 className="text-4xl md:text-6xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
                             NEW SHIRTS!
                         </h1>
-                        <h3 className="text-lg font-medium text-gray-200 mt-2">
+                        <h3 className="text-base md:text-lg font-medium text-gray-200 mt-2">
                             Only available for a limited time!
                         </h3>
-                        <a className="text-lg font-medium text-gray-200 bg-green-500 p-2 rounded-xl mt-6" href="https://staleyrobotics25.itemorder.com/">
+                        <a
+                            className="text-base md:text-lg font-medium text-gray-200 bg-green-500 p-2 md:p-4 rounded-xl mt-6"
+                            href="https://staleyrobotics25.itemorder.com/"
+                        >
                             Order Now!
                         </a>
                     </div>
                 </div>
                 <div
-                    className="absolute bottom-28 left-8 z-20 transition-transform duration-300"
-                >
-                    <div className="frosted-glass p-6 rounded-xl inline-block">
-                        <h1 className="text-8xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
+                    className="absolute bottom-12 left-4 md:bottom-28 md:left-8 z-20 transition-transform duration-300">
+                    <div className="frosted-glass p-4 md:p-6 rounded-xl inline-block">
+                        <h1 className="text-4xl md:text-8xl font-extrabold text-green-500 drop-shadow-[0_0_10px_rgba(0,255,0,0.7)] animate-pulse tracking-wide">
                             4959
                         </h1>
-                        <p className="text-lg font-medium text-gray-200 mt-2">
+                        <p className="text-sm md:text-lg font-medium text-gray-200 mt-2">
                             Millennium Falcons Robotics
                         </p>
                     </div>
                 </div>
             </div>
             <div className="content relative z-10">
-                <AboutSection />
+                <AboutSection/>
             </div>
         </div>
     );
@@ -52,21 +56,21 @@ const Landing: React.FC = () => {
 
 const AboutSection: React.FC = () => (
     <div
-        className="bg-gray-200/20 backdrop-blur-md rounded-2xl p-6 mx-4 my-6 max-h-[calc(100vh-40px)] overflow-y-auto shadow-md"
+        className="bg-gray-200/20 backdrop-blur-md rounded-2xl p-4 md:p-6 mx-4 my-6 max-h-[calc(100vh-40px)] overflow-y-auto shadow-md"
     >
-        <h1 className="text-2xl font-bold mb-4">About Us</h1>
-        <p className="text-white">
+        <h1 className="text-xl md:text-2xl font-bold mb-4">About Us</h1>
+        <p className="text-sm md:text-white">
             Staley Robotics, also known as the Millennium Falcons robotics team, is registered as team 4959 of FIRST
             Robotics. Since our founding in 2013, we have worked to develop a strong sense of community and form a basis
             for our team to continue to thrive and expand upon for years to come.
-            <br />
-            <br />
+            <br/>
+            <br/>
             Over the past ten years, our team has made it to The FIRST World Robotics Championships, won multiple times
             at the Kansas City Regional, in 2015 we won the judges award, and in 2023 our then head coach, Aaron Dalton,
             received the Woodie Flowers Award for his impact on our team. We hope to continue competing and building
             successful robots in years to come.
-            <br />
-            <br />
+            <br/>
+            <br/>
             <a href="/seasons" className="text-green-600 underline hover:text-green-800">
                 Past robots and events
             </a>
